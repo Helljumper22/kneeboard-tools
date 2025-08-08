b0705572c58957edea2ff5611c74e003ff128d37
Simon Pacotte <82233462+Helljumper22@users.noreply.github.com>
2025-08-08 18:16:54 +0200

    added Caucasus, GermanyCW and Nevada origin points

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 const mapsOrigin = {
+    'Caucasus': [45.1294, 34.2655],
+    'GermanyCW': [54.7005, 20.45],
     'Kola': [68.0, 23.5],
+    'Nevada': [39.8180, -114.7333],
     'PersianGulf': [26.1718, 56.2419],
 }
